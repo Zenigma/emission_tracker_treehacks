0544f8524dea08900016bff38bd287076518abf9
BrysonXiao <40129652+BrysonXiao@users.noreply.github.com>
2021-02-12 19:33:43 -0800

    Starting code

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav() {
+    return (
+      <nav>
+          <ul>
+              <li>Results</li>
+          </ul>
+      </nav>
+    );
+}
+
+export default Nav;
